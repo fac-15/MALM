9637467723bafc2ed63c2dca9318c1522539d7ef
mike <boimik87@gmail.com>
2018-11-29 17:33:23 +0000

    various changes

--- src/handlers.js
+++ src/handlers.js
@@ -17,16 +17,44 @@ const handleHomeRoute = (req, res) => {
   });
 };
 
-const handleApiCall =()=> {
+const handlePublic = (req, res, url) => {
+  const filePath = path.join(__dirname, "..", "public", url);
+  const ext = url.split("."[1]);
+  const extType = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    ico: "image/x-icon"
+  };
 
-  request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body.url);
-    console.log(body.explanation);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end(file);
+    } else {
+      res.writeHead(200, { "Content-Type": `${extType[ext]}` });
+      res.end(file);
+    }
   });
-}
+};
+
+const handleApiCall = () => {
+  request(
+    "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
+    { json: true },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(body.url);
+      console.log(body.explanation);
+    }
+  );
+};
 
 module.exports = {
   handleHomeRoute,
-  handleApiCall
+  handleApiCall,
+  handlePublic
 };
--- src/router.js
+++ src/router.js
@@ -5,11 +5,24 @@ const router = (req, res) => {
   const url = req.url;
   if (url === "/") {
     handlers.handleHomeRoute(req, res);
-    handlers.handleApiCall(); //TEST API 
+    handlers.handleApiCall(); //TEST API
+  } else if (routes.includes(url)) {
+    handlers.handlePublic(req, res, url);
+  } else if (url.indexOf("/search") !== -1) {
+    handlers.handleRequest(req, res);
   } else {
     res.writeHead(404, "Content-Type: text/html");
     res.end("<h1>404 File not found</h1>");
   }
 };
+
+const routes = [
+  "/main.css",
+  "/reset.css",
+  "dom.js",
+  "/request.js",
+  "/favicon.ico",
+  "/404"
+];
 
 module.exports = router;
